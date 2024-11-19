dffe86b10252fda4bb5314113a808edd8dfe2069
Michael(devlunch4) <devlunch4@gmail.com>
2024-11-20 00:02:50 +0900

    #4.2 Events (10:56)

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-const loginForm = docment.getElementById("login-form");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value)
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
