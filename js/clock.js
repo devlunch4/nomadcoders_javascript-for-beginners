6624af8af1dc9930a5a62228bf32304090b969b4
Michael(devlunch4) <devlunch4@gmail.com>
2024-12-19 01:56:41 +0900

    #5.2 PadStart (07:42)

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.minutes()).padStart(2, "0");
+    const secondes = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${secondes}`;
 }
 
 setInterval(getClock, 1000);
