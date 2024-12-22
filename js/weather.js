fdb5d98c5c57a8f5f1e2e51ea6ff0a8ffa3616c2
Michael(devlunch4) <devlunch4@gmail.com>
2024-12-22 18:19:53 +0900

    END

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,10 @@
-const API_KEY = "63ae468686fe94c4e330c6b969949f09"; // OpenWeatherMap API KEY
+const API_KEY = "63ae468686fe94c4e330c6b969949f09";
 
 function onGeoOk(position) {
-    const lat = position.coords.latitude; // 사용자의 위도
-    const lon = position.coords.longitude; // 사용자의 경도
+    //const lat = position.coords.latitude; // 사용자의 위도
+    //const lon = position.coords.longitude; // 사용자의 경도
+    const lat = 37; // 사용자의 위도
+    const lon = 126; // 사용자의 경도
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then((response) => response.json())
@@ -10,11 +12,11 @@ function onGeoOk(position) {
             const weather = document.querySelector("#weather");
             weather.innerText = `${data.name}: ${data.weather[0].main}, ${data.main.temp}°C`;
         });
-
 }
 
 function onGeoError() {
     alert("Can't find you. No weather for you.")
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+//navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+onGeoOk();
