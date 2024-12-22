6b7812e9ae530b345c4b2f725f427de5c1ffaa23
Michael(devlunch4) <devlunch4@gmail.com>
2024-12-22 17:41:18 +0900

    #8.1 Weather API (14:13)

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "63ae468686fe94c4e330c6b969949f09"; // OpenWeatherMap API KEY
+
 function onGeoOk(position) {
-const lat = position.coords.latitude;
-const lng = position.coords.longitude;
-console.log("You live in", lat, lng);
+    const lat = position.coords.latitude; // 사용자의 위도
+    const lon = position.coords.longitude; // 사용자의 경도
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            const name = data.name;
+            weather.innerText = data.weather[0].main;
+        });
 }
 
 function onGeoError() {
