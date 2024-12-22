ee295e60de571643e4169d48359eb1eed1e9a3ec
Michael(devlunch4) <devlunch4@gmail.com>
2024-12-22 19:34:34 +0900

    view modified

--- js/background.js
+++ js/background.js
@@ -11,9 +11,4 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
-// Add styles to adjust the image size
-bgImage.style.width = "auto"; // Fit the image to the width of the window
-bgImage.style.height = "auto"; // Maintain the aspect ratio
-bgImage.style.display = "block"; // Remove any default margin
-
 document.body.appendChild(bgImage);
